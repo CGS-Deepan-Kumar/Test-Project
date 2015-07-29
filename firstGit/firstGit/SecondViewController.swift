9972c474dba7946b18c738481565aab94fed5ebe
Deepan Kumar <17067@cgs.act.edu.au>
2015-07-29 18:59:19 +1000

    new view controller file added

--- firstGit/firstGit/SecondViewController.swift
+++ firstGit/firstGit/SecondViewController.swift
@@ -1,28 +1,26 @@
 //
-//  ViewController.swift
+//  SecondViewController.swift
 //  firstGit
 //
-//  Created by Deepan on 28/07/2015.
+//  Created by Deepan on 29/07/2015.
 //  Copyright (c) 2015 Deepan Kumar. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class SecondViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //random comment
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    //Comment
-    func featurex() {
-        //something new
-    }
 
+    
 }
 
